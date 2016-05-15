f20027219e47e384853b42d38d309d43a70f1877
Yusuke Yamada <yamachu@yamachuMBP.local>
2016-05-15 19:58:28 +0900

    use linq query

--- PowerPointSlideIndicator_2010/ThisAddIn.cs
+++ PowerPointSlideIndicator_2010/ThisAddIn.cs
@@ -33,22 +33,26 @@ namespace PowerPointSlideIndicator_2010
 
         private void Application_SlideShowNextClick(PowerPoint.SlideShowWindow Wn, PowerPoint.Effect nEffect)
         {
+            // ToDo: 変更できるようなインターフェース
+            const string INDICATOR_OBJECT = "indicator_obj";
+            const string SLIDE_NUMBER_OBJECT = "スライド番号プレースホルダー 3";
+
+
             var slide = Wn.View.Slide;
 
-            var slideMax = 0;
-            foreach (PowerPoint.Slide s in Wn.Presentation.Slides)
-                slideMax += s.SlideShowTransition.Hidden.Equals(Office.MsoTriState.msoFalse) ? 1 : 0;
+            var visible_slide = Wn.Presentation.Slides.Cast<Microsoft.Office.Interop.PowerPoint.Slide>().Count(
+                s => s.SlideShowTransition.Hidden.Equals(Office.MsoTriState.msoFalse));
             
             try
             {
-                var shapes = Wn.View.Slide.Shapes.Range("indicator_obj");
-                var indicator_width = shapes.Width;
-                var slide_num_obj = Wn.View.Slide.Shapes.Range("スライド番号プレースホルダー 3");
+                var indicator_obj = Wn.View.Slide.Shapes.Range(INDICATOR_OBJECT);
+                var indicator_width = indicator_obj.Width;
+                var slide_num_obj = Wn.View.Slide.Shapes.Range(SLIDE_NUMBER_OBJECT);
                 var left = slide_num_obj.Left;
                 // all - title
-                var delta = (left - indicator_width) / (slideMax - 2);
+                var delta = (left - indicator_width) / (visible_slide - 2);
                 // slide start at 2
-                shapes.Left = (slide.SlideNumber - 2) * delta;
+                indicator_obj.Left = (slide.SlideNumber - 2) * delta;
             }
             catch (Exception ex)
             {
